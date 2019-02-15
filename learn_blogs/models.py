9c2f64b100ef2bd7937b0538f41fdb832ba70f2f
jack.s <my_email@exp.co>
2019-02-15 16:53:02 +0800

    add entry

--- learn_blogs/models.py
+++ learn_blogs/models.py
@@ -9,4 +9,18 @@ class Topic(models.Model):
         return self.text
 
 
+class Entry(models.Model):
+    topic=models.ForeignKey(Topic,on_delete=models.CASCADE)
+    date=models.DateTimeField(auto_now_add=True)
+    text=models.TextField()
+
+    class Meta:
+        verbose_name_plural="Entries"
+
+    def __str__(self):
+        return self.text[:20]+"..."
+
+
+
+
 # Create your models here.
